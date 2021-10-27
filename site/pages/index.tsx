728c7d6bc6c150b3ca232204620c97ffd7197234
Zack Radisic <56137411+zackradisic@users.noreply.github.com>
2021-10-27 20:29:18 +0300

    update source code link

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -25,7 +25,7 @@ const Landing = ({ docs }: Props) => {
             <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
               <div className="rounded-md shadow">
                 <a
-                  href="#"
+                  href="https://github.com/zackradisic/aussieplusplus/"
                   className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
                   View source
                 </a>
